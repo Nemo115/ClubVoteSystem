e42eb71489583b23a6dbd072194efa28de803e88
Nemo-5 <stickman5464@gmail.com>
2024-04-04 18:04:29 +1100

    implementing database template

--- backend/club_vote_sys/flask-server/server.py
+++ backend/club_vote_sys/flask-server/server.py
@@ -2,13 +2,22 @@ from flask import Flask, render_template, request, redirect, flash, url_for
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://username:password@localhost/db_name' #'mysql://username:password@localhost/db_name'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db =  SQLAlchemy(app)
 
 #[---SQL TABLES---]
 class sample_table(db.Model):{
-
+    
+    #example constructor to be used
+    '''def __init__(self, username, email):
+        self.username = username
+        self.email = email'''
 }
+
+
+db.create_all()
 
 @app.route('/')
 def home():
